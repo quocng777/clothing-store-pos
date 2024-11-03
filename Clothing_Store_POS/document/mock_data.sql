bebe6a5f50ba68c08adfd8181644102e66f9f85d
tyanfarm <phamquangtuyen.nt@gmail.com>
2024-11-03 22:42:36 +0700

    fix UI in LoginPage

--- Clothing_Store_POS/document/mock_data.sql
+++ Clothing_Store_POS/document/mock_data.sql
@@ -13,6 +13,7 @@ INSERT INTO products (name, price, category_id, size, color, stock, sale) VALUES
 ('Áo khoác', 600000, 1, 'L', 'Xám', 25, 0.05); -- Tham chiếu đến category_id 1
 
 INSERT INTO users (fullname, username, password_hash, email, user_role, is_active, created_date) VALUES
+    ('admin', 'admin', '$2a$11$SFExVrqW6dW8I/bzMyRKdeoG49HMk/xAu39lZ2x/HxPJQHHKzcr5S', 'admin@gmail.com', 'admin', TRUE, CURRENT_TIMESTAMP),
     ('John Sena', 'johnsena', '$2a$11$ygrhLkennieuLLcm/RBLlefPMWEigNb1PAdo6/yTknmgsDjYAl51.', 'johnsena@gmail.com', 'admin', TRUE, CURRENT_TIMESTAMP),
     ('Tyanin Poo', 'tyanipo', '$2a$11$KXJxNbhY607P9qY/g9DafO0.HoD2DPStza4ryE3TbsQphCL0MoFE.', 'tyanipo@gmail.com', 'admin', TRUE, CURRENT_TIMESTAMP),
     ('Kim Jong Un', 'kimjongun', '$2a$11$iY3rc9rfeUQoCyMUc9cQ6O58PzAar1cuC2GrhxRmtjPZfSK.m.N9C', 'kimjongun@gmail.com', 'admin', TRUE, CURRENT_TIMESTAMP);
