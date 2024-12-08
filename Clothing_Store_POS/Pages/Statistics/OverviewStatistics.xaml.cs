78b315ceef230fbf6d26ad68e3a438942658b7ce
21120331 <anhthai733029@gmail.com>
2024-12-08 19:49:14 +0700

    feat: overview statistics

---
+++ Clothing_Store_POS/Pages/Statistics/OverviewStatistics.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Clothing_Store_POS.ViewModels;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+using Microsoft.UI.Xaml.Controls;
+using QuestPDF.Helpers;
+using System.Collections.Generic;
+using Clothing_Store_POS.Converters;
+using System;
+using System.Diagnostics;
+
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Clothing_Store_POS.Pages.Statistics
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OverviewStatistics : Page
+    {
+        public SalesViewModel ViewModel { get; set; }
+
+        public OverviewStatistics()
+        {
+            this.InitializeComponent();
+            ViewModel = new SalesViewModel();
+            _ = ViewModel.LoadData();
+            DataContext = ViewModel;
+            LoadOverviewChart();
+        }
+
+        private async void OnYearSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await ViewModel.LoadSalesData(ViewModel.SelectedYear);
+            LoadOverviewChart();
+        }
+
+        private void LoadOverviewChart()
+        {
+            IncomeChart.Series = ViewModel.IncomeChartSeries;
+            IncomeChart.XAxes = new List<Axis>
+            {
+                new Axis
+                {
+                    Labels = ViewModel.Months,
+                    Labeler = value =>
+                    {
+                        int monthIndex = (int)value;
+                        if (monthIndex >= 0 && monthIndex < ViewModel.Months.Count)
+                        {
+                            return ViewModel.Months[monthIndex];
+                        }
+                        return string.Empty;
+                    }
+                }
+            };
+            IncomeChart.YAxes = new List<Axis>
+            {
+                new Axis
+                {
+                    Labeler = value => PriceToVNDConverter.convertToVND(value),
+                }
+            };
+        }
+    }
+}
---
+++ Clothing_Store_POS/Services/Statistics/OrderService.cs
@@ -0,0 +1,70 @@
+﻿using Clothing_Store_POS.Config;
+using Clothing_Store_POS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clothing_Store_POS.Services.Statistics
+{
+    public class OrderService
+    {
+        private readonly AppDBContext _context;
+        public OrderService()
+        {
+            _context = new AppDBContext();
+        }
+
+        public async Task<List<int>> GetAvailableYearsAsync()
+        {
+            var list = await _context.Orders
+                .Select(o => o.CreatedAt.Year)
+                .Distinct()
+                .OrderByDescending(y => y)
+                .ToListAsync();
+
+            return list;
+        }
+
+        public async Task<YearlySalesDto> GetYearlySalesAsync(int year)
+        {
+            var orders = await _context.Orders
+                .Where(o => o.CreatedAt.Year == year)
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .ToListAsync();
+
+            var monthlySales = new List<MonthlySalesDto>();
+
+            for (int month = 1; month <= 12; month++)
+            {
+                var monthlyIncome = orders
+                    .Where(o => o.CreatedAt.Month == month)
+                    .SelectMany(o => o.OrderItems)
+                    .Sum(oi => oi.Quantity * oi.Product.Price * (1 - oi.DiscountPercentage / 100));
+
+                monthlySales.Add(new MonthlySalesDto
+                {
+                    Month = month,
+                    TotalIncome = monthlyIncome
+                });
+            }
+
+            var totalSales = monthlySales.Sum(ms => ms.TotalIncome);
+
+            var topPerformingMonth = monthlySales
+                .OrderByDescending(ms => ms.TotalIncome)
+                .FirstOrDefault();
+
+            return new YearlySalesDto
+            {
+                Year = year,
+                MonthlySales = monthlySales,
+                TotalSales = totalSales,
+                TopPerformingMonth = topPerformingMonth
+            };
+        }
+    }
+}
