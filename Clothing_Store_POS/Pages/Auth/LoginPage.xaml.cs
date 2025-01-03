62311688243ca8e5feeba65d565f535f6af59794
tyanfarm <phamquangtuyen.nt@gmail.com>
2025-01-03 15:59:48 +0700

    debug

--- Clothing_Store_POS/Pages/Auth/LoginPage.xaml.cs
+++ Clothing_Store_POS/Pages/Auth/LoginPage.xaml.cs
@@ -1,21 +1,10 @@
 using Clothing_Store_POS.Config;
 using Clothing_Store_POS.Helper;
-using Clothing_Store_POS.Models;
 using Clothing_Store_POS.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +15,7 @@ namespace Clothing_Store_POS.Pages.Auth
     public sealed partial class LoginPage : Page
     {
         public UsersViewModel ViewModel { get; }
+
         public LoginPage()
         {
             this.InitializeComponent();
@@ -42,17 +32,10 @@ namespace Clothing_Store_POS.Pages.Auth
             }
         }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            base.OnNavigatedFrom(e);
-
-            UsernameTextBox.KeyDown -= TextBox_KeyDown;
-        }
-
         private async void Login_Click(object sender, RoutedEventArgs e)
         {
             {
-                UsernameErrorText.Visibility = Visibility.Collapsed;    
+                UsernameErrorText.Visibility = Visibility.Collapsed;
                 PasswordErrorText.Visibility = Visibility.Collapsed;
                 LoginErrorText.Visibility = Visibility.Collapsed;
             }
